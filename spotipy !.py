783b0e295ae28566ed0fe6b565d5085dc85a0f19
zuri-mckee <zcmckee@aggies.ncat.edu>
2025-02-03 17:44:49 -0500

    messing around with spotipy

---
+++ spotipy !.py
@@ -0,0 +1,14 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="c7e1d7b3540f4d9287407c5a47b450b7",
+                                                           client_secret="a6a75e851e5a4cd59ae05d69c1c6f029"))
+
+#results = sp.search(q='dangelo', limit=7)
+#for idx, track in enumerate(results['tracks']['items']):
+    #print(idx, track['name'])
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " - ", track['name'])
